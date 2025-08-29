888cc8fde2aa594b9bc152007d31942d42f56c44
ManuelaV2408 <villada.sadep@gmail.com>
2025-08-28 23:07:57 -0500

    Add files via upload

---
+++ semillero-m-portfolio-2025-main/src/App.jsx
@@ -0,0 +1,19 @@
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Home from "./pages/home";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <main>
+        <br />
+        <br />
+        <Home />
+      </main>
+      <Footer />
+    </>
+  );
+}
+
+export default App;
